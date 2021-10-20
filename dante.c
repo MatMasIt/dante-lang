9abdcddea3cd004a981894013cf182dbe9cb06e6
MatMasIt <onlinehomework@altervista.org>
2021-10-21 00:13:40 +0200

    Transpiler stub

---
+++ dante.c
@@ -0,0 +1,218 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+char *replace(
+    char const *const original,
+    char const *const pattern,
+    char const *const replacement)
+{
+    size_t const replen = strlen(replacement);
+    size_t const patlen = strlen(pattern);
+    size_t const orilen = strlen(original);
+
+    size_t patcnt = 0;
+    const char *oriptr;
+    const char *patloc;
+
+    // find how many times the pattern occurs in the original string
+    for (oriptr = original; patloc = strstr(oriptr, pattern); oriptr = patloc + patlen)
+    {
+        patcnt++;
+    }
+
+    {
+        // allocate memory for the new string
+        size_t const retlen = orilen + patcnt * (replen - patlen);
+        char *const returned = (char *)malloc(sizeof(char) * (retlen + 1));
+
+        if (returned != NULL)
+        {
+            // copy the original string,
+            // replacing all the instances of the pattern
+            char *retptr = returned;
+            for (oriptr = original; patloc = strstr(oriptr, pattern); oriptr = patloc + patlen)
+            {
+                size_t const skplen = patloc - oriptr;
+                // copy the section until the occurence of the pattern
+                strncpy(retptr, oriptr, skplen);
+                retptr += skplen;
+                // copy the replacement
+                strncpy(retptr, replacement, replen);
+                retptr += replen;
+            }
+            // copy the rest of the string.
+            strcpy(retptr, oriptr);
+        }
+        return returned;
+    }
+}
+char **str_split(char *a_str, const char a_delim)
+{
+    char **result = 0;
+    size_t count = 0;
+    char *tmp = a_str;
+    char *last_comma = 0;
+    char delim[2];
+    delim[0] = a_delim;
+    delim[1] = 0;
+
+    /* Count how many elements will be extracted. */
+    while (*tmp)
+    {
+        if (a_delim == *tmp)
+        {
+            count++;
+            last_comma = tmp;
+        }
+        tmp++;
+    }
+
+    /* Add space for trailing token. */
+    count += last_comma < (a_str + strlen(a_str) - 1);
+
+    /* Add space for terminating null string so caller
+       knows where the list of returned strings ends. */
+    count++;
+
+    result = malloc(sizeof(char *) * count);
+
+    if (result)
+    {
+        size_t idx = 0;
+        char *token = strtok(a_str, delim);
+
+        while (token)
+        {
+            assert(idx < count);
+            *(result + idx++) = strdup(token);
+            token = strtok(0, delim);
+        }
+        assert(idx == count - 1);
+        *(result + idx) = 0;
+    }
+
+    return result;
+}
+int prefix(const char *pre, const char *str)
+{
+    return strncmp(pre, str, strlen(pre)) == 0;
+}
+int countats(char *line)
+{
+    int tot = 0;
+    for (int i = 0; i < strlen(line); i++)
+    {
+        if (line[i] == '@')
+        {
+            tot++;
+            continue;
+        }
+
+        if (line[i] == ' ' || line[i] == '\t')
+            continue;
+
+        return tot;
+    }
+
+    return tot;
+}
+char *strstrip(char *s)
+{
+    size_t size;
+    char *end;
+
+    size = strlen(s);
+
+    if (!size)
+        return s;
+
+    end = s + size - 1;
+    while (end >= s && isspace(*end))
+        end--;
+    *(end + 1) = '\0';
+
+    while (*s && isspace(*s))
+        s++;
+
+    return s;
+}
+int main(void)
+{
+    FILE *fp;
+    FILE *fo;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    char **tokens;
+
+    fp = fopen("demo.dante", "r");
+    fo = fopen("out.c", "w+");
+    if (fp == NULL || fo == NULL)
+        exit(EXIT_FAILURE);
+
+    int prevAts = 0;
+    while ((read = getline(&line, &len, fp)) != -1)
+    {
+
+        int addSemicolon = 1;
+        char *copy = strdup(line);
+        int ats = countats(copy);
+        if (ats < prevAts)
+        {
+            printf("}");
+        }
+        else if (ats > prevAts)
+        {
+            printf("{");
+        }
+        prevAts = ats;
+        if (strlen(strstrip(copy)) == 0)
+            addSemicolon = 0;
+        tokens = str_split(line, '"');
+        for (int i = 0; *(tokens + i); i++)
+        {
+            if (!i % 2 || *(tokens + i + 1) == NULL)
+            {
+                char *newstr = replace(*(tokens + i), "restituisci", "return");
+                if (strstr(newstr, "principale") != NULL)
+                {
+                    addSemicolon = 0;
+                    newstr = replace(newstr, "principale", "int main()");
+                }
+                newstr = replace(newstr, "intero", "int");
+                if (strstr(newstr, "finchè ") != NULL)
+                {
+                    addSemicolon = 0;
+                    newstr = replace(newstr, "finchè", "while(");
+                    strcat(strstrip(newstr), " )\n");
+                }
+                for(int i=0;i<strlen(newstr);i++){
+                    if(newstr[i]!='@') printf("%c",newstr[i]);
+                }
+                //printf("%s", strstrip(newstr));
+            }
+            else
+            {
+                printf("\"%s\"", *(tokens + i));
+            }
+            free(*(tokens + i));
+        }
+        if (addSemicolon)
+            printf(";");
+        printf("\n");
+
+        /*
+        printf("%s",replace(line, "intero", "int"));
+        strcpy(line,replace(line, "decimale", "double"));
+        strcpy(line,replace(line, "testo", "char * "));
+        strcpy(line,replace(line, "funzione", ""));
+        */
+        fprintf(fo, "%s", line);
+    }
+
+    fclose(fp);
+    if (line)
+        free(line);
+    exit(EXIT_SUCCESS);
+}
---
+++ out.c
@@ -0,0 +1,9 @@
+funzione somma(intero a,intero b)  restituisce intero
+@   stampa(@	restituisci a+b
+
+
+principale
+@   intero a = 0
+@   finchè a < 10
+@   @   stampa(@   @   a++
+
