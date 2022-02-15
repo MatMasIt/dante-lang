b1a3e195acb14f0dd8d5f6bf7e579602b0c95076
MatMasIt <onlinehomework@altervista.org>
2022-02-15 15:37:30 +0100

    Added hfile tree and object

---
+++ syntaxTree/object.c
@@ -0,0 +1,5 @@
+typedef struct Object{
+    
+} Object;
+
+
---
+++ syntaxTree/syntaxTree.h
@@ -0,0 +1,8 @@
+#include "expression.c"
+#include "for.c"
+#include "if.c"
+#include "object.c"
+#include "stree.c"
+#include "token.c";
+#include "variable.c"
+#include "while.c"
