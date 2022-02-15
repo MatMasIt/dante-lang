f29d164c177b459ae971693f3524a127543741eb
MatMasIt <onlinehomework@altervista.org>
2022-02-15 10:56:51 +0100

    New syntax tree model in progress, acces descriptors

---
+++ syntaxTree/stree.c
@@ -0,0 +1,12 @@
+typedef struct Stree {
+    Stree *parent;
+    ArrayList children; //Stree **children;
+    Token token;
+} Stree;
+
+STree sTreeInit(Token token, Stree *parent){
+    Stree a;
+    a.token = token;
+    a.parent = parent;
+    a.children = arraylist_new();
+}
--- tokens.h
+++ tokens.h
@@ -1,5 +1,4 @@
 #define MAIN_T "inizio"
 #define IF_T "se"
 #define WHILE_T "finchè"
-#define token int
 // define MAX(a, b) (((a) > (b)) ? (a) : (b))
